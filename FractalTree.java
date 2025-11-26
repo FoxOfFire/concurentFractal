5728dbd9f80fb3666208599a34867dad3b0c3a86
Balázs Vince <54199935+FoxOfFire@users.noreply.github.com>
2025-11-26 20:34:24 +0100

    skibidi

--- FractalTree.java
+++ FractalTree.java
@@ -1,116 +1,53 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 
 public class FractalTree extends Canvas {
 	/* Variables with class-wide visibility */
-	private static boolean slowMode;
+	private final Transfer trans;
+	private int linesDrawn = 0;
 
-	private static final int capacity = 10;
-	private static int lineCount = 0;
-	private static final BlockingQueue<Line> queue = new ArrayBlockingQueue<>(capacity);
-	private static boolean producerActive = true;
-
-	private Color createColor(int height) {
-		int moder = 3;
-		int red = 3 - height % moder;
-		int green = 3 - (height / moder) % moder;
-		int blue = 3 - (height / (moder * moder)) % moder;
-		red = red * 64 + 63;
-		green = green * 64 + 63;
-		blue = blue * 64 + 63;
-
-		return new Color(red, green, blue);
+	private FractalTree(Transfer trans) {
+		this.trans = trans;
 	}
 
-	/* Recursive function for calculating all drawcalls for the fractal tree */
-	public static void makeFractalTree(int x, int y, int angle, int height) {
-
-		if (slowMode) {
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException ie) {
-				ie.printStackTrace();
-			}
-		}
-
-		if (height == 0)
-			return;
-
-		int x2 = x + (int) (Math.cos(Math.toRadians(angle)) * height * 7);
-		int y2 = y + (int) (Math.sin(Math.toRadians(angle)) * height * 7);
-		try {
-
-			Line l = new Line(x, y, x2, y2);
-			FractalTree.queue.put(l);
-		} catch (InterruptedException ex) {
-
-			System.err.println("makeFractalTree was interupted");
-		}
-
-		makeFractalTree(x2, y2, angle - 20, height - 1);
-		makeFractalTree(x2, y2, angle + 20, height - 1);
-	}
-
-	public void drawFractalSegment(Graphics g) {
-		try {
-			while (producerActive) {
-				g.setColor(createColor(FractalTree.lineCount));
-				Line l = FractalTree.queue.take();
-				l.draw(g);
-				FractalTree.lineCount += 1;
-
-			}
-		} catch (InterruptedException ex) {
-		}
-	}
-
-	/* Code for EDT */
-	/* Must only contain swing code (draw things on the screen) */
-	/* Must not contain calculations (do not use math and compute libraries here) */
-	/*
-	 * No need to understand swing, a simple endless loop that draws lines is enough
-	 */
 	@Override
 	public void paint(Graphics g) {
-		drawFractalSegment(g);
+		while (!trans.consumerDone()) {
+			try {
+				trans.nextLine().draw(g);
+				linesDrawn++;
+			} catch (InterruptedException ex) {
+				System.err.println("interupted");
+			}
 
-		try {
-			Thread.sleep(200);
-		} catch (InterruptedException ie) {
-
-			ie.printStackTrace();
 		}
+		System.out.println("linesDrawn:" + linesDrawn);
 	}
 
 	/* Code for main thread */
 	public static void main(String args[]) {
 
 		/* Parse args */
-		slowMode = args.length != 0 && Boolean.parseBoolean(args[0]);
 
 		/* Initialize graphical elements and EDT */
-		Thread segmentProducer = new Thread(() -> {
-			makeFractalTree(390, 480, -90, 11);
-			producerActive = false;
-			System.out.println("producer has finished");
+		int x = 1000;
+		int y = 1000;
+		int height = 16;
 
-		});
-		Thread drawingConsumer = new Thread(() -> {
-			FractalTree tree = new FractalTree();
-			JFrame frame = new JFrame();
-			frame.setSize(800, 600);
-			frame.setVisible(true);
-			frame.setResizable(false);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.getContentPane().setBackground(new Color(20, 20, 20));
-			frame.add(tree);
+		Transfer trans = new Transfer(height);
 
-			System.out.println("consumer has finished");
-		});
-		segmentProducer.start();
-		drawingConsumer.start();
+		ExecutorService service = new ExecutorService(x / 2, y / 20, 90, height, trans);
+
+		FractalTree tree = new FractalTree(trans);
+		JFrame frame = new JFrame();
+		frame.setSize(x, y);
+		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setBackground(new Color(20, 20, 20));
+		frame.add(tree);
+
+		service.start();
 		/* Log success as last step */
 		System.out.println("Main has finished");
 	}
--- Line.java
+++ Line.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics;
 
 public class Line {
@@ -5,19 +6,34 @@ public class Line {
 	public int x2;
 	public int y1;
 	public int y2;
+	public int height;
 	public boolean drawn = false;
 
-	public Line(int x1, int y1, int x2, int y2) {
+	public Line(int x1, int y1, int x2, int y2, int height) {
 		this.x1 = x1;
 		this.x2 = x2;
 		this.y1 = y1;
 		this.y2 = y2;
+		this.height = height;
+	}
+
+	private Color createColor(int height) {
+		int moder = 3;
+		int red = 3 - height % moder;
+		int green = 3 - (height / moder) % moder;
+		int blue = 3 - (height / (moder * moder)) % moder;
+		red = red * 64 + 63;
+		green = green * 64 + 63;
+		blue = blue * 64 + 63;
+
+		return new Color(red, green, blue);
 	}
 
 	public void draw(Graphics g) {
 		if (drawn)
 			return;
 
+		g.setColor(createColor(height));
 		g.drawLine(x1, y1, x2, y2);
 		drawn = true;
 	}
